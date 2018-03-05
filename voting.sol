2e8f0567d60b2d323a4353bffa59d46c7d99c529
shreyaspatil221 <35215633+shreyaspatil221@users.noreply.github.com>
2018-03-05 20:44:05 +0530

    2.0

--- voting.sol
+++ voting.sol
@@ -1,29 +1,27 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.20;
 
 
 contract Voting {
  
   
   mapping (bytes32 => uint8) public votesReceived;
-  
- 
-  
   bytes32[] public candidateList;
-
- 
-  function Voting(bytes32[] candidateNames) {
+  
+  function Voting(bytes32[] candidateNames) public{
     candidateList = candidateNames;
   }
 
+
+
   
-  function totalVotesFor(bytes32 candidate) returns (uint8) {
-    if (validCandidate(candidate) == false) throw;
+  function totalVotesFor(bytes32 candidate)public returns (uint8) {
+    if (validCandidate(candidate) == false) revert();
     return votesReceived[candidate];
   }
 
   
   function voteForCandidate(bytes32 candidate) {
-    if (validCandidate(candidate) == false) throw;
+    if (validCandidate(candidate) == false) revert();
     votesReceived[candidate] += 1;
   }
 
